8b7fd3c7588980fcdde2331f74af2c3b5f136e3c
AdrjGo <gcadrianj@gmail.com>
2024-06-13 00:53:46 -0400

    update index, update components, add fonts

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,12 +3,26 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     fontFamily: {
-      sans: ["Poppins", "sans-serif"],
+      sans: [
+        "Universal-Sans-Display-Medium",
+        "Universal-Sans-Display-Regular",
+        "Universal-Sans-Text-Bold",
+        "Universal-Sans-Text-Medium",
+        "Universal-Sans-Text-Regular",
+        "sans-serif" // Fuente de respaldo
+      ],
     },
     extend: {
       colors: {
         "light-grey": "#393C41",
-        "dark-grey":"#171A20"
+        "dark-grey": "#171A20",
+      },
+      fontFamily: {
+        "display-regular": ["Universal-Sans-Display-Regular", "sans-serif"],
+        "display-medium": ["Universal-Sans-Display-Medium", "sans-serif"],
+        "text-bold": ["Universal-Sans-Text-Bold", "sans-serif"],
+        "text-medium": ["Universal-Sans-Text-Medium", "sans-serif"],
+        "text-regular": ["Universal-Sans-Text-Regular", "sans-serif"],
       },
     },
   },
